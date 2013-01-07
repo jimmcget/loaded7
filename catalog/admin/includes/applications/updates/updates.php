04641612aabdedef664963196212b30bc1a0c337
datazen <scott.logsdon@loadedcommerce.com>
2013-01-07 16:35:48 -0500

    #223 core updates - adding logic for core updater

--- catalog/admin/includes/applications/updates/updates.php
+++ catalog/admin/includes/applications/updates/updates.php
@@ -1,17 +1,15 @@
 <?php
 /*
-  $Id: updates.php v1.0 2013-01-01 datazen $
+  $Id: updates.php v1.0 2011-11-04 datazen $
 
   LoadedCommerce, Innovative eCommerce Solutions
   http://www.loadedcommerce.com
 
-  Copyright (c) 2013 Loaded Commerce, LLC
+  Copyright (c) 2011 LoadedCommerce.com
 
   @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
+  @copyright  (c) 2011 LoadedCommerce Team
   @license    http://loadedcommerce.com/license.html
-
-  @function The lC_Application_Services class manages the addons GUI
 */
 class lC_Application_Updates extends lC_Template_Admin {
   /*
@@ -27,7 +25,6 @@ class lC_Application_Updates extends lC_Template_Admin {
     global $lC_Language;
 
     $this->_page_title = $lC_Language->get('heading_title');
-
   }
 }
 ?>
--- catalog/admin/includes/modules/access/updates.php
+++ catalog/admin/includes/modules/access/updates.php
@@ -1,25 +1,25 @@
 <?php
 /*
-  $Id: updates.php v1.0 2013-01-01 datazen $
+  $Id: updates.php v1.0 2011-11-04 datazen $
 
   LoadedCommerce, Innovative eCommerce Solutions
   http://www.loadedcommerce.com
 
-  Copyright (c) 2013 Loaded Commerce, LLC
+  Copyright (c) 2011 LoadedCommerce.com
 
   @author     LoadedCommerce Team
-  @copyright  (c) 2013 LoadedCommerce Team
+  @copyright  (c) 2011 LoadedCommerce Team
   @license    http://loadedcommerce.com/license.html
 */
 
-  class lC_Access_Updater extends lC_Access {
+  class lC_Access_Updates extends lC_Access {
     var $_module = 'updates',
-        $_group = 'hidden',
-        $_icon = 'coreupdate.png',
+        $_group = 'tools',
+        $_icon = 'people.png',
         $_title,
-        $_sort_order = 800;
+        $_sort_order = 100;
 
-    function lC_Access_Updater() {
+    function lC_Access_Updates() {
       global $lC_Language;
 
       $this->_title = $lC_Language->get('access_updates_title');
