d6c07fc1c6a2ed27b9754972bee3ac8059fea33a
geraldbullard <gerald.bullard@loadedcommerce.com>
2013-01-08 15:15:09 -0500

    commit testing 123

--- catalog/templates/default/content/checkout/checkout_shipping.php
+++ catalog/templates/default/content/checkout/checkout_shipping.php
@@ -11,6 +11,7 @@
   @copyright  (c) 2013 LoadedCommerce Team
   @license    http://loadedcommerce.com/license.html
   */
+  // added for commit testing to new branch
 ?>
 <!--CHECKOUT SHIPPING SECTION STARTS-->
   <div id="checkout_shipping_details" class="full_page">
